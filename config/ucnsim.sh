b237cfd3337e86bcb87412240f31933dd3054f0f
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:07:58 +0000

    Cleaned up config files
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@32 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -4,32 +4,31 @@
 echo "------------------------------------------------------------------------"
 echo "Setting up environment for UCNSIM"
 echo "------------------------------------------------------------------------"
-export SOFT_ROOT=/Users/sjp39/Software
-export UCN_ROOT=${SOFT_ROOT}/ucnsim/trunk
-export UCN_GEOM=${UCN_ROOT}/geom
+export UCNSIM=${HOME}/Projects/git/ucnsim
+export UCN_GEOM=${UCNSIM}/geom
 #------------------------------------------------------------------------------
 # ROOT
 #------------------------------------------------------------------------------
-export ROOTSYS=${SOFT_ROOT}/ROOT/v5.24.00
-export ROOTMACROS=${UCN_ROOT}/macros:${SOFT_ROOT}/macros
-export ROOTLOGON=${SOFT_ROOT}/config/ucn_rootlogon.C
-export ROOTLOGOFF=${SOFT_ROOT}/config/ucn_rootlogoff.C
+export ROOTMACROS=${UCNSIM}/macros:${ROOTSYS}/macros
+export ROOTALIAS=${UCNSIM}/config/ucn_rootalias.C
+export ROOTLOGON=${UCNSIM}/config/ucn_rootlogon.C
+export ROOTLOGOFF=${UCNSIM}/config/ucn_rootlogoff.C
 #------------------------------------------------------------------------------
 # PATHS
 #------------------------------------------------------------------------------
-export PATH=${UCN_ROOT}/bin:$ROOTSYS/bin:$PATH
+export PATH=${UCNSIM}/bin:$PATH
 if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
-export LD_LIBRARY_PATH=${ROOTSYS}/lib:${UCN_ROOT}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${UCNSIM}/lib:${LD_LIBRARY_PATH}
 else
-export LD_LIBRARY_PATH=${ROOTSYS}/lib:${UCN_ROOT}/lib
+export LD_LIBRARY_PATH=${UCNSIM}/lib
 fi
 if [[ -n "${DYLD_LIBRARY_PATH}" ]] ; then
-export DYLD_LIBRARY_PATH=${ROOTSYS}/lib:${UCN_ROOT}/lib:${DYLD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=${UCNSIM}/lib:${DYLD_LIBRARY_PATH}
 else
-export DYLD_LIBRARY_PATH=${ROOTSYS}/lib:${UCN_ROOT}/lib
+export DYLD_LIBRARY_PATH=${UCNSIM}/lib
 fi
 #------------------------------------------------------------------------------
-echo "UCN   : " $UCN_ROOT
+echo "UCN   : " $UCNSIM
 echo "ROOT  : " $ROOTSYS
 echo "------------------------------------------------------------------------"
 #------------------------------------------------------------------------------
