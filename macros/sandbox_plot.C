04290713c439a9ee30cbb755d28e3755bd34a03d
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:09:12 +0000

    Updated sandbox with previous work on buildtests
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@148 7850984a-0f19-49c7-84f0-416d0ede6f3e

---
+++ macros/sandbox_plot.C
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+#include <stdio.h> // sprintf
+
+#include "TCanvas.h"
+#include "TBenchmark.h"
+#include "TSystem.h"
+#include "TRint.h"
+#include "TRandom.h"
+#include "TObjArray.h"
+#include "TFile.h"
+#include "TKey.h"
+
+#include "TF1.h"
+#include "TH1.h"
+#include "TStyle.h"
+#include "TMath.h"
+#include "TLegend.h"
+#include "TGraphErrors.h"
+
+#include "TGeoManager.h"
+#include "TGeoNavigator.h"
+#include "TGeoTrack.h"
+#include "TParticle.h"
+#include "TParticlePDG.h"
+#include "TGeoMatrix.h"
+#include "TPolyMarker3D.h"
+#include "TVectorD.h"
+
+#include "include/Constants.h"
+#include "include/Units.h"
+#include "include/FitSuite.h"
+
+using std::vector;
+
+Bool_t DrawInitialAndFinalPositions(const char* fileName, TGeoManager* geoManager);
+Bool_t DrawInitialAndFinalDirections(const char* fileName, TGeoManager* geoManager); 
+
+Int_t sandbox_plot(const char* fileName) {
+	
+	// -- Import Geometry
+	TGeoManager* geoManager = TGeoManager::Import(fileName); 
+	// Needs to be imported first because we draw the volumes in certain histograms from it, so we do not want it
+	// deleted in each function. 
+	
+	DrawInitialAndFinalPositions(fileName, geoManager);
+	DrawInitialAndFinalDirections(fileName, geoManager);
+	
+	return 0;
+}
+
+// -------------------------------------------------------------------------------------- 
+Bool_t DrawInitialAndFinalPositions(const char* fileName, TGeoManager* geoManager) 
+{
+	// -- Open File
+	TFile *file = 0;
+	file = TFile::Open(fileName, "read");
+	if (!file || file->IsZombie()) {
+	   cerr << "Cannot open file: " << fileName << endl;
+	   return 0;
+	}
+	// -- Extract Initial and Final Positions in TPolyMarker3D
+	const char* initialFileName = "NeutronInitialPositions;1";
+	const char* finalFileName = "NeutronFinalPositions;1";
+	TPolyMarker3D* initialPositions = new TPolyMarker3D();
+	TPolyMarker3D* finalPositions = new TPolyMarker3D();
+	file->GetObject(initialFileName, initialPositions);
+	if (initialPositions == NULL) {
+		cerr << "Could not find TPolyMarker3D: " << initialFileName << endl;
+		return kFALSE;
+	}
+	file->GetObject(finalFileName, finalPositions);
+	if (finalPositions == NULL) {
+		cerr << "Could not find TPolyMarker3D: " << finalFileName << endl;
+		return kFALSE;
+	}
+	
+	// -- Draw Initial Points
+	TCanvas *canvas = new TCanvas("InitialAndFinalPositionsCanvas","Neutron Initial and Final Positions",60,0,400,400);
+	canvas->Divide(2,1);
+	canvas->cd(1);
+	geoManager->GetTopVolume()->Draw();
+	geoManager->SetVisLevel(4);
+	geoManager->SetVisOption(0);
+	initialPositions->Draw();
+	// -- Draw Final Points
+	canvas->cd(2);
+	geoManager->GetTopVolume()->Draw();
+	geoManager->SetVisLevel(4);
+	geoManager->SetVisOption(0);
+	finalPositions->Draw();
+	
+	return kTRUE;
+}
+
+// -------------------------------------------------------------------------------------- 
+Bool_t DrawInitialAndFinalDirections(const char* fileName, TGeoManager* geoManager) 
+{
+	// -- Open File
+	TFile *file = 0;
+	file = TFile::Open(fileName, "read");
+	if (!file || file->IsZombie()) {
+	   cerr << "Cannot open file: " << fileName << endl;
+	   return 0;
+	}
+	// -- Extract Histogram Objects
+	const char* initialThetaHistName = "InitialThetaHist;1";
+	const char* initialPhiHistName = "InitialPhiHist;1";
+	const char* finalThetaHistName = "FinalThetaHist;1";
+	const char* finalPhiHistName = "FinalPhiHist;1";
+	TH1F* initialThetaHist = new TH1F();
+	TH1F* initialPhiHist = new TH1F();
+	TH1F* finalThetaHist = new TH1F();
+	TH1F* finalPhiHist = new TH1F();
+   file->GetObject(initialThetaHistName, initialThetaHist);
+	file->GetObject(initialPhiHistName, initialPhiHist);
+   file->GetObject(finalThetaHistName, finalThetaHist);
+   file->GetObject(finalPhiHistName, finalPhiHist);
+	if (initialThetaHist == NULL || initialPhiHist == NULL || finalThetaHist == NULL || finalPhiHist == NULL) {
+		cerr << "Could not find required histogram" << endl;
+		return kFALSE;
+	}
+	// -- Draw Histograms
+	TCanvas *canvas = new TCanvas("InitialAndFinalDirections","Neutron Initial and Final Directions",60,0,800,800);
+	canvas->Divide(2,2);
+	canvas->cd(1);
+	initialThetaHist->Draw();
+	canvas->cd(2);
+	initialPhiHist->Draw();
+	canvas->cd(3);
+	finalThetaHist->Draw();
+	canvas->cd(4);
+	finalPhiHist->Draw();
+	
+	return kTRUE;
+}
+
--- src/sandbox.cxx
+++ src/sandbox.cxx
@@ -54,7 +54,8 @@ using std::endl;
 using std::cerr;
 using std::string;
 
-Bool_t LoadFile(const string& configFileName, TFile* dataFile);
+Bool_t PlotInitialAndFinalPositions(const TString& dataFileName, const TString& runName);
+Bool_t PlotInitialAndFinalAngularDistribution(const TString& dataFileName, const TString& runName);
 
 Int_t main(Int_t argc,Char_t **argv)
 {
@@ -95,13 +96,139 @@ Int_t main(Int_t argc,Char_t **argv)
 		return EXIT_FAILURE;
 	}
 	
-//	navigator->DiffuseCoefficient(0.1);
-//	benchmark.Stop("UCNSim");
-//	benchmark.Show("UCNSim");
-//	theApp->Run();
-
 	cout << endl << endl << "END OF SIMULATION" << endl << endl << endl;
 	delete experiment;		
 	
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Run Analysis of Data
+	///////////////////////////////////////////////////////////////////////////////////////
+	
+	TUCNConfigFile configFile(configFileName);
+	TString dataFile(configFile.GetString("OutputFile","I/O"));
+	
+	cout << endl << endl;
+	cout << "-------------------------------------------" << endl;
+	cout << "Run Build Test Analysis on: " << dataFile <<  endl;
+	cout << "-------------------------------------------" << endl << endl;
+	
+	PlotInitialAndFinalPositions(dataFile, "Run1;1");
+	PlotInitialAndFinalAngularDistribution(dataFile, "Run1;1");
+	
 	return EXIT_SUCCESS;
 }
+
+// -------------------------------------------------------------------------------------- 
+Bool_t PlotInitialAndFinalPositions(const TString& dataFileName, const TString& runName) 
+{
+// -- Create a TPolyMarker3D object to store the final positions of the neutrons and write this to file. 
+	cout << "-------------------------------------------" << endl;
+	cout << "DrawInitialAndFinalPositions" <<  endl;
+	cout << "-------------------------------------------" << endl;
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Open File
+	TFile *file = 0;
+	file = TFile::Open(dataFileName, "update");
+	if (!file || file->IsZombie()) {
+	   cerr << "Cannot open file: " << dataFileName << endl;
+	   return 0;
+	}
+	// -- Extract Run Object
+	TUCNRun* run = new TUCNRun();
+   file->GetObject(static_cast<const char*>(runName), run);
+	if (run == NULL) {
+		cerr << "Could not find run: " << runName << endl;
+		return kFALSE;
+	}
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Create the initial points
+	TPolyMarker3D* initialPoints = new TPolyMarker3D(run->Neutrons(), 1); // 1 is marker style
+	for (Int_t i = 0; i < run->Neutrons(); i++) {
+		TUCNParticle* particle = run->GetInitialParticle(i);
+		assert(particle != NULL);
+		initialPoints->SetPoint(i, particle->Vx(), particle->Vy(), particle->Vz());
+	}
+	initialPoints->SetMarkerColor(2);
+	initialPoints->SetMarkerStyle(6);
+	// -- Write the points to the File
+	initialPoints->SetName("NeutronInitialPositions");
+	initialPoints->Write();
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Create the final points
+	TPolyMarker3D* finalPoints = new TPolyMarker3D(run->Neutrons(), 1); // 1 is marker style
+	for (Int_t i = 0; i < run->Neutrons(); i++) {
+		TUCNParticle* particle = run->GetParticle(i);
+		assert(particle != NULL);
+		finalPoints->SetPoint(i, particle->Vx(), particle->Vy(), particle->Vz());
+	}
+	finalPoints->SetMarkerColor(2);
+	finalPoints->SetMarkerStyle(6);
+	// -- Write the points to the File
+	finalPoints->SetName("NeutronFinalPositions");
+	finalPoints->Write();
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Clean Up
+	delete run;
+	delete initialPoints; 
+	delete finalPoints;
+	delete file;
+	
+	return kTRUE;
+}
+
+// -------------------------------------------------------------------------------------- 
+Bool_t PlotInitialAndFinalAngularDistribution(const TString& dataFileName, const TString& runName) 
+{
+// -- Create a Histogram object to store the angular distribution (as in, their initial and final directions about the origin). 
+	cout << "-------------------------------------------" << endl;
+	cout << "DrawInitialAndFinalAngularDistribution" <<  endl;
+	cout << "-------------------------------------------" << endl;
+	///////////////////////////////////////////////////////////////////////////////////////
+	// -- Open File
+	TFile *file = 0;
+	file = TFile::Open(dataFileName, "update");
+	if (!file || file->IsZombie()) {
+	   cerr << "Cannot open file: " << dataFileName << endl;
+	   return 0;
+	}
+	// -- Extract Run Object
+	TUCNRun* run = new TUCNRun();
+   file->GetObject(static_cast<const char*>(runName), run);
+	if (run == NULL) {
+		cerr << "Could not find run: " << runName << endl;
+		return kFALSE;
+	}
+	// -- Plot the Initial and Final Directions
+	Int_t nbins = 50;
+	TH1F* initialThetaHist = new TH1F("InitialThetaHist","Initial Direction: Theta component, Units of Pi", nbins, 0.0, 1.0);
+	TH1F* initialPhiHist = new TH1F("InitialPhiHist","Initial Direction: Phi component, Units of Pi", nbins, 0.0, 2.0);
+	TH1F* finalThetaHist = new TH1F("FinalThetaHist","Final Direction: Theta component, Units of Pi", nbins, 0.0, 1.0);
+	TH1F* finalPhiHist = new TH1F("FinalPhiHist","Final Direction: Phi component, Units of Pi", nbins, 0.0, 2.0);
+	// Axis Titles
+	initialThetaHist->SetXTitle("Height from bottom of Tube (m)");
+	initialThetaHist->SetYTitle("Number of Neutrons");
+	
+	for (Int_t i = 0; i < run->Neutrons(); i++) {
+		TUCNParticle* initialParticle = run->GetInitialParticle(i);
+		TUCNParticle* finalParticle = run->GetParticle(i);
+		initialThetaHist->Fill(initialParticle->Theta()/TMath::Pi());
+		initialPhiHist->Fill(initialParticle->Phi()/TMath::Pi());
+		finalThetaHist->Fill(finalParticle->Theta()/TMath::Pi());
+		finalPhiHist->Fill(finalParticle->Phi()/TMath::Pi());
+	}
+	// -- Write the points to the File
+	initialThetaHist->Write();
+	initialPhiHist->Write();
+	finalThetaHist->Write();
+	finalPhiHist->Write();
+	
+	delete initialThetaHist;
+	delete initialPhiHist;
+	delete finalThetaHist;
+	delete finalPhiHist;
+	delete run;
+	delete file;
+	
+	return kTRUE;
+}
+
+
