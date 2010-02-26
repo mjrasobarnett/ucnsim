577ce19efcc1f8bff1f9fee710345a3f72a3739a
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:09:22 +0000

    Added ROOT's code to CompositeShape classes
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@163 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- include/TUCNGeoBoolNode.h
+++ include/TUCNGeoBoolNode.h
@@ -1,4 +1,4 @@
-// @(#):$Id: TGeoBoolNode.h 25878 2008-10-18 19:12:08Z rdm $
+// @(#):$Id: TUCNGeoBoolNode.h 25878 2008-10-18 19:12:08Z rdm $
 // Author: Andrei Gheata   30/05/02
 
 /*************************************************************************
@@ -14,6 +14,10 @@
 
 #ifndef ROOT_TObject
 #include "TObject.h"
+#endif
+
+#ifndef ROOT_TGeoMatrix
+#include "TGeoMatrix.h"
 #endif
 
 //////////////////////////////////////////////////////////////////////////////
@@ -32,6 +36,55 @@ class TGeoHMatrix;
 
 class TUCNGeoBoolNode : public TObject
 {
+public:
+enum EGeoBoolType {
+   kGeoUnion,
+   kGeoIntersection,
+   kGeoSubtraction
+};
+private:
+   TUCNGeoBoolNode(const TUCNGeoBoolNode&); // Not implemented
+   TUCNGeoBoolNode& operator=(const TUCNGeoBoolNode&); // Not implemented
+
+protected:
+   Int_t             fSelected;       // ! selected branch
+   TGeoShape        *fLeft;           // shape on the left branch
+   TGeoShape        *fRight;          // shape on the right branch
+   TGeoMatrix       *fLeftMat;        // transformation that applies to the left branch
+   TGeoMatrix       *fRightMat;       // transformation that applies to the right branch
+// methods
+   Bool_t            MakeBranch(const char *expr, Bool_t left);
+public:
+   // constructors
+   TUCNGeoBoolNode();
+   TUCNGeoBoolNode(const char *expr1, const char *expr2);
+   TUCNGeoBoolNode(TGeoShape *left, TGeoShape *right, TGeoMatrix *lmat=0, TGeoMatrix *rmat=0);
+
+   // destructor
+   virtual ~TUCNGeoBoolNode();
+   // methods
+   virtual void      ComputeBBox(Double_t &dx, Double_t &dy, Double_t &dz, Double_t *origin) = 0;
+   virtual void      ComputeNormal(Double_t *point, Double_t *dir, Double_t *norm) = 0;
+   virtual Bool_t    Contains(Double_t *point) const         = 0;
+   virtual Int_t     DistanceToPrimitive(Int_t px, Int_t py) = 0;
+   virtual Double_t  DistFromInside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const = 0;
+   virtual Double_t  DistFromOutside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const = 0;
+   virtual EGeoBoolType GetBooleanOperator() const = 0;
+   virtual Int_t     GetNpoints() const = 0;
+   TGeoMatrix       *GetLeftMatrix() const {return fLeftMat;}
+   TGeoMatrix       *GetRightMatrix() const {return fRightMat;}
+   TGeoShape        *GetLeftShape() const {return fLeft;}
+   TGeoShape        *GetRightShape() const {return fRight;}
+   virtual void      Paint(Option_t *option);
+   void              RegisterMatrices();
+   virtual Double_t  Safety(Double_t *point, Bool_t in=kTRUE) const = 0;
+   virtual void      SavePrimitive(ostream &out, Option_t *option = "");
+   virtual void      SetPoints(Double_t *points) const = 0;
+   virtual void      SetPoints(Float_t *points)  const = 0;
+   void              SetSelected(Int_t sel) {fSelected = sel;}
+   virtual void      Sizeof3D() const;
 
    ClassDef(TUCNGeoBoolNode, 1)              // a boolean node
 };
@@ -45,6 +98,32 @@ class TUCNGeoBoolNode : public TObject
 class TUCNGeoUnion : public TUCNGeoBoolNode
 {
 public:
+   // constructors
+   TUCNGeoUnion();
+   TUCNGeoUnion(const char *expr1, const char *expr2);
+   TUCNGeoUnion(TGeoShape *left, TGeoShape *right, TGeoMatrix *lmat=0, TGeoMatrix *rmat=0);
+
+   // destructor
+   virtual ~TUCNGeoUnion();
+   // methods
+   virtual void      ComputeBBox(Double_t &dx, Double_t &dy, Double_t &dz, Double_t *origin);
+   virtual void      ComputeNormal(Double_t *point, Double_t *dir, Double_t *norm);
+   virtual Bool_t    Contains(Double_t *point) const;
+   virtual Int_t     DistanceToPrimitive(Int_t px, Int_t py);
+   virtual Double_t  DistFromInside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual Double_t  DistFromOutside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual EGeoBoolType GetBooleanOperator() const {return kGeoUnion;}
+   virtual Int_t     GetNpoints() const;
+   virtual Double_t  Safety(Double_t *point, Bool_t in=kTRUE) const;
+   virtual void      SavePrimitive(ostream &out, Option_t *option = "");
+   virtual void      SetPoints(Double_t *points) const;
+   virtual void      SetPoints(Float_t *points)  const;
+   virtual void      Sizeof3D() const;
+
+   //CS specific
+   virtual void      Paint(Option_t *option);
 
    ClassDef(TUCNGeoUnion, 1)              // union node
 };
@@ -59,6 +138,32 @@ public:
 class TUCNGeoIntersection : public TUCNGeoBoolNode
 {
 public:
+   // constructors
+   TUCNGeoIntersection();
+   TUCNGeoIntersection(const char *expr1, const char *expr2);
+   TUCNGeoIntersection(TGeoShape *left, TGeoShape *right, TGeoMatrix *lmat=0, TGeoMatrix *rmat=0);
+
+   // destructor
+   virtual ~TUCNGeoIntersection();
+   // methods
+   virtual void      ComputeBBox(Double_t &dx, Double_t &dy, Double_t &dz, Double_t *origin);
+   virtual void      ComputeNormal(Double_t *point, Double_t *dir, Double_t *norm);
+   virtual Bool_t    Contains(Double_t *point) const;
+   virtual Int_t     DistanceToPrimitive(Int_t px, Int_t py);
+   virtual Double_t  DistFromInside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual Double_t  DistFromOutside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual EGeoBoolType GetBooleanOperator() const {return kGeoIntersection;}
+   virtual Int_t     GetNpoints() const;
+   virtual Double_t  Safety(Double_t *point, Bool_t in=kTRUE) const;
+   virtual void      SavePrimitive(ostream &out, Option_t *option = "");
+   virtual void      SetPoints(Double_t *points) const;
+   virtual void      SetPoints(Float_t *points)  const;
+   virtual void      Sizeof3D() const;
+
+   //CS specific
+   virtual void      Paint(Option_t *option);
 
    ClassDef(TUCNGeoIntersection, 1)              // intersection node
 };
@@ -72,7 +177,32 @@ public:
 class TUCNGeoSubtraction : public TUCNGeoBoolNode
 {
 public:
+   // constructors
+   TUCNGeoSubtraction();
+   TUCNGeoSubtraction(const char *expr1, const char *expr2);
+   TUCNGeoSubtraction(TGeoShape *left, TGeoShape *right, TGeoMatrix *lmat=0, TGeoMatrix *rmat=0);
 
+   // destructor
+   virtual ~TUCNGeoSubtraction();
+   // methods
+   virtual void      ComputeBBox(Double_t &dx, Double_t &dy, Double_t &dz, Double_t *origin);
+   virtual void      ComputeNormal(Double_t *point, Double_t *dir, Double_t *norm);
+   virtual Bool_t    Contains(Double_t *point) const;
+   virtual Int_t     DistanceToPrimitive(Int_t px, Int_t py);
+   virtual Double_t  DistFromInside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual Double_t  DistFromOutside(Double_t *point, Double_t *dir, Int_t iact=1,
+                               Double_t step=0, Double_t *safe=0) const;
+   virtual EGeoBoolType GetBooleanOperator() const {return kGeoSubtraction;}
+   virtual Int_t     GetNpoints() const;
+   virtual Double_t  Safety(Double_t *point, Bool_t in=kTRUE) const;
+   virtual void      SavePrimitive(ostream &out, Option_t *option = "");
+   virtual void      SetPoints(Double_t *points) const;
+   virtual void      SetPoints(Float_t *points)  const;
+   virtual void      Sizeof3D() const;
+
+   //CS specific
+   virtual void      Paint(Option_t *option);
 
    ClassDef(TUCNGeoSubtraction, 1)              // subtraction node
 };
--- include/TUCNGeoCompositeShape.h
+++ include/TUCNGeoCompositeShape.h
@@ -24,11 +24,64 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
-//class TUCNGeoBoolNode;
+class TUCNGeoBoolNode;
 
 class TUCNGeoCompositeShape : public TUCNGeoBBox
 {
+private :
+// data members
+   TUCNGeoBoolNode         *fNode;             // top boolean node
 
+protected:
+   TUCNGeoCompositeShape(const TUCNGeoCompositeShape& gcs)
+     : TUCNGeoBBox(gcs), fNode(gcs.fNode) { }
+   TUCNGeoCompositeShape& operator=(const TUCNGeoCompositeShape& gcs)
+     {if(this!=&gcs) {TUCNGeoBBox::operator=(gcs); fNode=gcs.fNode;} return *this;}
+
+public:
+   // constructors
+   TUCNGeoCompositeShape();
+   TUCNGeoCompositeShape(const char *name, const char *expression);
+   TUCNGeoCompositeShape(const char *expression);
+   TUCNGeoCompositeShape(const char *name, TUCNGeoBoolNode *node);
+   // destructor
+   virtual ~TUCNGeoCompositeShape();
+   // methods
+   virtual Double_t      Capacity() const;
+   virtual void          ComputeBBox();
+   virtual void          ComputeNormal(Double_t *point, Double_t *dir, Double_t *norm);
+   virtual Bool_t        Contains(Double_t *point) const;
+   virtual Double_t      DistFromInside(Double_t *point, Double_t *dir, Int_t iact=1, 
+                                   Double_t step=TGeoShape::Big(), Double_t *safe=0) const;
+   virtual Double_t      DistFromOutside(Double_t *point, Double_t *dir, Int_t iact=1, 
+                                   Double_t step=TGeoShape::Big(), Double_t *safe=0) const;
+   virtual TGeoVolume   *Divide(TGeoVolume *voldiv, const char *divname, Int_t iaxis, Int_t ndiv, 
+                                Double_t start, Double_t step);
+   TUCNGeoBoolNode      *GetBoolNode() const {return fNode;}
+   virtual void          GetBoundingCylinder(Double_t * /*param*/) const {;}
+   virtual TGeoShape    *GetMakeRuntimeShape(TGeoShape * /*mother*/, TGeoMatrix * /*mat*/) const {return 0;}
+   virtual void          GetMeshNumbers(Int_t &nvert, Int_t &nsegs, Int_t &npols) const;
+   virtual Int_t         GetNmeshVertices() const;
+   virtual Bool_t        GetPointsOnSegments(Int_t /*npoints*/, Double_t * /*array*/) const {return kFALSE;}
+   virtual void          InspectShape() const;
+   virtual Bool_t        IsComposite() const {return kTRUE;}
+   virtual Bool_t        IsCylType() const {return kFALSE;}
+   void                  MakeNode(const char *expression);
+   virtual Bool_t        PaintComposite(Option_t *option = "") const;
+   void                  RegisterYourself();
+   virtual Double_t      Safety(Double_t *point, Bool_t in=kTRUE) const;
+   virtual void          SavePrimitive(ostream &out, Option_t *option = "");
+   virtual void          SetDimensions(Double_t * /*param*/) {;}
+   virtual void          SetPoints(Double_t *points) const;
+   virtual void          SetPoints(Float_t *points) const;
+   virtual void          Sizeof3D() const;
+
+	// methods
+//	virtual Double_t		TimeFromInsideAlongParabola(Double_t* point, Double_t* velocity, Double_t* field, 
+//																			Double_t stepmax=TGeoShape::Big(), Int_t iact=1, Double_t *safe=0) const;
+//	virtual Double_t		TimeFromOutsideAlongParabola(Double_t* point, Double_t* velocity, Double_t* field, 
+//																			Double_t stepmax=TGeoShape::Big(), Int_t iact=1, Double_t *safe=0) const;
+		
    ClassDef(TUCNGeoCompositeShape, 1)         // boolean composite shape
 };
 
