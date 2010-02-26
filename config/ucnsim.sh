5fb9a587ce0f39b712a6c24ece385a5ac1078462
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:08:53 +0000

    Added Simon's config details to setup script
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@116 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -9,27 +9,30 @@ if [[ `uname -n` == "MJRasoBarnett.local" ]] ; then
 	# First test if we are running on matt's machine
 	echo "Configuring for Matt's local machine"
 	export UCNSIM=${HOME}/Projects/git/ucnsim
+	export UCN_GEOM=${UCNSIM}/geom
 	if [[ -n "${ROOTSYS}" ]] ; then
 		export ROOTSYS=${HOME}/Apps/ROOT/root
-		export PATH=${ROOTSYS}/bin:${PATH}
+		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
-	fi	
+	fi
 elif [[ `uname -n` == "sjp39.local" ]] ; then # Need Simon's local hostname
 	# Test if we are running on simon's machine
 	echo "Configuring for Simon's local machine"
-	export UCNSIM=${HOME} # Simon to fill in here where he stores the code	
+	export UCNSIM=${HOME}/Software/ucnsim/trunk # Simon to fill in here where he stores the code	
+	export UCN_GEOM=${UCNSIM}/geom
 	if [[ -n "${ROOTSYS}" ]] ; then
-		export ROOTSYS=/usr/local/bin # Simon to fill in where he keeps his root
-		export PATH=${ROOTSYS}/bin:${PATH}
+		export ROOTSYS=${HOME}Software/ROOT/v5.24.00 # Simon to fill in where he keeps his root
+		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
-	fi	
+	fi
 elif [[ `uname -n` == "neutrino.pact.cpes.susx.ac.uk" ]] ; then 
 	# Test if we are running on neutrino
 	echo "Configuring for neutrino remote machine"
 	export UCNSIM=/export/eppcode/ucnsim
+	export UCN_GEOM=${UCNSIM}/geom
 	if [[ -n "${ROOTSYS}" ]] ; then
 		export ROOTSYS=/export/eppcode/root_v5.22.00
-		export PATH=${ROOTSYS}/bin:${PATH}
+		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
 	fi
 	# Add more machines here as necessary
@@ -37,50 +40,36 @@ elif [[ `uname -n` == "ubuntu" ]] ; then
 	# Test if we are running on matt's linux virtual machine
 	echo "Configuring for Matt's linux virtual machine"
 	export UCNSIM=${HOME}/code/git/ucnsim
+	export UCN_GEOM=${UCNSIM}/geom
 	if [[ -n "${ROOTSYS}" ]] ; then
 		export ROOTSYS=${HOME}/code/root
-		export PATH=${ROOTSYS}/bin:${PATH}
+		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
 	fi
-fi	
-
-export UCN_GEOM=${UCNSIM}/geom
+fi
 
 #------------------------------------------------------------------------------
-# ROOT
+# ROOT & UCNSIM PATHS
 #------------------------------------------------------------------------------
 if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ROOTSYS}/lib
+	export LD_LIBRARY_PATH=${UCNSIM}/lib:${ROOTSYS}/lib:${LD_LIBRARY_PATH}
 else
-	export LD_LIBRARY_PATH=${ROOTSYS}/lib
+	export LD_LIBRARY_PATH=${UCNSIM}/lib:${ROOTSYS}/lib:
 fi
 
 if [[ `uname` == Darwin ]] ; then
   if [[ -n "${DYLD_LIBRARY_PATH}" ]] ; then
-   export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${ROOTSYS}/lib
+   export DYLD_LIBRARY_PATH=${UCNSIM}/lib:${ROOTSYS}/lib:${DYLD_LIBRARY_PATH}
   else
-   export DYLD_LIBRARY_PATH=${ROOTSYS}/lib
+   export DYLD_LIBRARY_PATH=${UCNSIM}/lib:${ROOTSYS}/lib:
   fi
 fi
+
 export ROOTMACROS=${UCNSIM}/macros:${ROOTSYS}/macros
 export ROOTALIAS=${UCNSIM}/config/ucn_rootalias.C
 export ROOTLOGON=${UCNSIM}/config/ucn_rootlogon.C
 export ROOTLOGOFF=${UCNSIM}/config/ucn_rootlogoff.C
 
-#------------------------------------------------------------------------------
-# UCNSIM PATHS
-#------------------------------------------------------------------------------
-export PATH=${UCNSIM}/bin:$PATH
-if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
-export LD_LIBRARY_PATH=${UCNSIM}/lib:${LD_LIBRARY_PATH}
-else
-export LD_LIBRARY_PATH=${UCNSIM}/lib
-fi
-if [[ -n "${DYLD_LIBRARY_PATH}" ]] ; then
-export DYLD_LIBRARY_PATH=${UCNSIM}/lib:${DYLD_LIBRARY_PATH}
-else
-export DYLD_LIBRARY_PATH=${UCNSIM}/lib
-fi
 #------------------------------------------------------------------------------
 echo "UCN   : " $UCNSIM
 echo "ROOT  : " $ROOTSYS
