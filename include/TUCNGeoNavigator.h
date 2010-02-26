42415d5d6c2bc25fc260b47e548eade4ef332f8c
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:09:35 +0000

    Removed all code from Navigator
    -- Class is left hollow for now, in preparation to delete at a future revision when I am sure I will never use this again
    
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@178 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- include/TUCNGeoNavigator.h
+++ include/TUCNGeoNavigator.h
@@ -29,57 +29,6 @@ class TGeoHMatrix;
 class TUCNGeoNavigator : public TGeoNavigator 
 {
 
-protected:
-	TUCNGeoNavigator(const TUCNGeoNavigator&); 
- 	TUCNGeoNavigator& operator=(const TUCNGeoNavigator&);
-	
-	static const Double_t 		fgTolerance = 1.E-10;
-		
-	TGeoNode*						fUCNNextNode;
-	Double_t							fUCNNormal[3];
-	
-	Double_t					 		fStepTime;			  
-	
-	Bool_t 							fUCNIsStepEntering;
-	Bool_t 							fUCNIsStepExiting;
-	Bool_t							fUCNIsOutside;        //! flag that current point is outside geometry
-	Bool_t							fUCNIsOnBoundary;     //! flag that current point is on some boundary	
-	
-public:
-	// -- constructors
-   TUCNGeoNavigator();
-   TUCNGeoNavigator(TGeoManager* geom);
-   
-	// -- destructor
-	virtual ~TUCNGeoNavigator();
-
-	// -- methods
-	Bool_t                  IsUCNStepEntering() const       {return fUCNIsStepEntering;}
-   Bool_t                  IsUCNStepExiting() const        {return fUCNIsStepExiting;}
-   Bool_t                  IsUCNOutside() const            {return fUCNIsOutside;}
-   Bool_t                  IsUCNOnBoundary() const         {return fUCNIsOnBoundary;}
-	
-	// New methods to find the normal vector of fUCNNextNode, instead of fNextNode
-	Double_t*					FindUCNNormal();				
-	const Double_t*			GetNormal() const        			{return fUCNNormal;}
-   
-	// Track Propagation methods
-	TGeoNode*					FindNextDaughterBoundaryAlongParabola(Double_t* point, Double_t* velocity, 
-																							Double_t* field, Int_t &idaughter, Bool_t compmatrix=kFALSE);
- 	TGeoNode*					FindNextBoundaryAndStepAlongParabola(TVirtualGeoTrack* track, TUCNGravField* field,
- 																							Double_t stepTime, Bool_t compsafe=kFALSE);
-		
-	Double_t                DetermineNextStepTime(TUCNParticle* particle, const Double_t maxStepTime, const Double_t runTime=0.);
-	Double_t 					GetStepTime() const {return fStepTime;}
-	void							SetStepTime(Double_t stepTime); 
-	
-	Bool_t						MakeStep(TVirtualGeoTrack* track, TUCNGravField* gravField=0, TUCNMagField* magField=0);
-	
-	Bool_t 						Bounce(TVirtualGeoTrack* track, const Double_t* normal, TUCNGeoMaterial* wallMaterial);
-	Bool_t 						SpecularBounce(Double_t* dir, const Double_t* norm);
-	Bool_t 						DiffuseBounce(Double_t* dir, const Double_t* norm);
-	void							UpdateTrack(TVirtualGeoTrack* track, Double_t timeInterval=0., TUCNGravField* gravField=0);
-	
    ClassDef(TUCNGeoNavigator, 1)          // UCN geometry navigator
 };
 
