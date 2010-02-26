75724fb5051d1f5bcd4fb0764a8c6b179ac2b95f
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-02-26 15:08:25 +0000

    Attempt at a universal configuration file
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@77 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -4,17 +4,62 @@
 echo "------------------------------------------------------------------------"
 echo "Setting up environment for UCNSIM"
 echo "------------------------------------------------------------------------"
-export UCNSIM=${HOME}/Projects/git/ucnsim
+
+if [[ `uname -n` == "MJRasoBarnett.local" ]] ; then
+	# First test if we are running on matt's machine
+	echo "Configuring for Matt's local machine"
+	export UCNSIM=${HOME}/Projects/git/ucnsim
+	if [[ -n "${ROOTSYS}" ]] ; then
+		export ROOTSYS=${HOME}/Apps/ROOT/root
+		export PATH=${ROOTSYS}/bin:${PATH}
+		export MANPATH=${ROOTSYS}/man:${MANPATH}
+	fi	
+elif [[ `uname -n` == "sjp39.local" ]] ; then # Need Simon's local hostname
+	# Test if we are running on simon's machine
+	echo "Configuring for Simon's local machine"
+	export UCNSIM=${HOME} # Simon to fill in here where he stores the code	
+	if [[ -n "${ROOTSYS}" ]] ; then
+		export ROOTSYS=/usr/local/bin # Simon to fill in where he keeps his root
+		export PATH=${ROOTSYS}/bin:${PATH}
+		export MANPATH=${ROOTSYS}/man:${MANPATH}
+	fi	
+elif [[ `uname -n` == "neutrino.pact.cpes.susx.ac.uk" ]] ; then 
+	# Test if we are running on neutrino
+	echo "Configuring for neutrino remote machine"
+	export UCNSIM=/export/eppcode/ucnsim
+	if [[ -n "${ROOTSYS}" ]] ; then
+		export ROOTSYS=/export/eppcode/root_v5.22.00
+		export PATH=${ROOTSYS}/bin:${PATH}
+		export MANPATH=${ROOTSYS}/man:${MANPATH}
+	fi
+	# Add more machines here as necessary
+fi	
+
 export UCN_GEOM=${UCNSIM}/geom
+
 #------------------------------------------------------------------------------
 # ROOT
 #------------------------------------------------------------------------------
+if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ROOTSYS}/lib
+else
+	export LD_LIBRARY_PATH=${ROOTSYS}/lib
+fi
+
+if [[ `uname` == Darwin ]] ; then
+  if [[ -n "${DYLD_LIBRARY_PATH}" ]] ; then
+   export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${ROOTSYS}/lib
+  else
+   export DYLD_LIBRARY_PATH=${ROOTSYS}/lib
+  fi
+fi
 export ROOTMACROS=${UCNSIM}/macros:${ROOTSYS}/macros
 export ROOTALIAS=${UCNSIM}/config/ucn_rootalias.C
 export ROOTLOGON=${UCNSIM}/config/ucn_rootlogon.C
 export ROOTLOGOFF=${UCNSIM}/config/ucn_rootlogoff.C
+
 #------------------------------------------------------------------------------
-# PATHS
+# UCNSIM PATHS
 #------------------------------------------------------------------------------
 export PATH=${UCNSIM}/bin:$PATH
 if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
