a78f3aa127d4722d94ed0f97841101564472443d
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-03-31 14:44:52 +0000

    Further tweaks in ucnsim.sh aimed at neutrino
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@304 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -4,13 +4,14 @@
 echo "------------------------------------------------------------------------"
 echo "Setting up environment for UCNSIM"
 echo "------------------------------------------------------------------------"
+export null = ""
 
 if [[ `uname -n` == "MJRasoBarnett.local" ]] ; then
 	# First test if we are running on matt's machine
 	echo "Configuring for Matt's local machine"
 	export UCNSIM=${HOME}/Projects/git/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS:-unset}" ]] ; then
+	if [[ -n "${ROOTSYS:-null}" ]] ; then
 		export ROOTSYS=${HOME}/Apps/ROOT/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -20,7 +21,7 @@ elif [[ `uname -n` == "sjp39.local" ]] ; then # Need Simon's local hostname
 	echo "Configuring for Simon's local machine"
 	export UCNSIM=${HOME}/Software/ucnsim/trunk # Simon to fill in here where he stores the code	
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS:-unset}" ]] ; then
+	if [[ -n "${ROOTSYS:-null}" ]] ; then
 		export ROOTSYS=${HOME}Software/ROOT/v5.24.00 # Simon to fill in where he keeps his root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -30,7 +31,7 @@ elif [[ `uname -n` == "neutrino.pact.cpes.susx.ac.uk" ]] ; then
 	echo "Configuring for neutrino remote machine"
 	export UCNSIM=/export/eppcode/matt/dev/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS:-unset}" ]] ; then
+	if [[ -n "${ROOTSYS:-null}" ]] ; then
 		export ROOTSYS=/export/eppcode/matt/root/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -41,7 +42,7 @@ elif [[ `uname -n` == "ubuntu" ]] ; then
 	echo "Configuring for Matt's linux virtual machine"
 	export UCNSIM=${HOME}/code/git/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS:-unset}" ]] ; then
+	if [[ -n "${ROOTSYS:-null}" ]] ; then
 		export ROOTSYS=${HOME}/code/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
