5d4a97d0dc706858844c6bd6d892ff63d8e4cc82
mb325 <mb325@7850984a-0f19-49c7-84f0-416d0ede6f3e>
2010-03-31 13:53:19 +0000

    Change to ucnsim.sh to help setup on neutrino
    -- Encountered a problem when sourcing ucnsim.sh on neutrino where bash complained that $ROOTSYS was an unbound variable.
    
    git-svn-id: svn+ssh://ssh.pact.cpes.susx.ac.uk/export/eppcode/matt/dev/svn/ucnsim@302 7850984a-0f19-49c7-84f0-416d0ede6f3e

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -10,7 +10,7 @@ if [[ `uname -n` == "MJRasoBarnett.local" ]] ; then
 	echo "Configuring for Matt's local machine"
 	export UCNSIM=${HOME}/Projects/git/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS}" ]] ; then
+	if [[ -n "${ROOTSYS:-unset}" ]] ; then
 		export ROOTSYS=${HOME}/Apps/ROOT/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -20,7 +20,7 @@ elif [[ `uname -n` == "sjp39.local" ]] ; then # Need Simon's local hostname
 	echo "Configuring for Simon's local machine"
 	export UCNSIM=${HOME}/Software/ucnsim/trunk # Simon to fill in here where he stores the code	
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS}" ]] ; then
+	if [[ -n "${ROOTSYS:-unset}" ]] ; then
 		export ROOTSYS=${HOME}Software/ROOT/v5.24.00 # Simon to fill in where he keeps his root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -30,7 +30,7 @@ elif [[ `uname -n` == "neutrino.pact.cpes.susx.ac.uk" ]] ; then
 	echo "Configuring for neutrino remote machine"
 	export UCNSIM=/export/eppcode/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS}" ]] ; then
+	if [[ -n "${ROOTSYS:-unset}" ]] ; then
 		export ROOTSYS=/export/eppcode/matt/root/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
@@ -41,7 +41,7 @@ elif [[ `uname -n` == "ubuntu" ]] ; then
 	echo "Configuring for Matt's linux virtual machine"
 	export UCNSIM=${HOME}/code/git/ucnsim
 	export UCN_GEOM=${UCNSIM}/geom
-	if [[ -n "${ROOTSYS}" ]] ; then
+	if [[ -n "${ROOTSYS:-unset}" ]] ; then
 		export ROOTSYS=${HOME}/code/root
 		export PATH=${UCNSIM}/bin:${ROOTSYS}/bin:${PATH}
 		export MANPATH=${ROOTSYS}/man:${MANPATH}
