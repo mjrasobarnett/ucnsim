2a2b0955df998427e530a3f50dcf021691b8b7a3
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2010-10-01 12:55:43 +0100

    Another minor alreation of comments in Materials.h

--- include/Materials.h
+++ include/Materials.h
@@ -195,9 +195,9 @@ namespace Materials
       TUCNElement* elemHydrogen = new TUCNElement("H", "Hydrogen", Hydrogen::z, Hydrogen::a, Hydrogen::scatLength, Hydrogen::cohCrossSec, Hydrogen::incohCrossSec, Hydrogen::absCrossSec);
       TUCNMaterial* matHydrogen = new TUCNMaterial("Hydrogen", elemHydrogen, Densities::hydrogen);
       TGeoMedium* hydrogen = new TGeoMedium("Hydrogen", 1, matHydrogen);
+      cout << "Added Material: " << hydrogen->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << hydrogen->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::hydrogen << endl;
       cout << "Coherent Cross Sec: " << elemHydrogen->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemHydrogen->InCoherentCrossSec() << endl;
@@ -215,9 +215,9 @@ namespace Materials
       TGeoMedium* beryllium = new TGeoMedium("Beryllium", 1, matBeryllium);
       // Set Beryllium Loss factor to the experimentally determined value
       matBeryllium->LossFactor(LossFactor::beryllium);
+      cout << "Added Material: " << beryllium->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << beryllium->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::beryllium << endl;
       cout << "Coherent Cross Sec: " << elemBeryllium->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemBeryllium->InCoherentCrossSec() << endl;
@@ -233,9 +233,9 @@ namespace Materials
       TUCNElement* elemCopper = new TUCNElement("Cu", "Copper", Copper::z, Copper::a, Copper::scatLength, Copper::cohCrossSec, Copper::incohCrossSec, Copper::absCrossSec);
       TUCNMaterial* matCopper = new TUCNMaterial("Copper", elemCopper, Densities::copper);
       TGeoMedium* copper = new TGeoMedium("Copper", 1, matCopper);
+      cout << "Added Material: " << copper->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << copper->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::copper << endl;
       cout << "Coherent Cross Sec: " << elemCopper->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemCopper->InCoherentCrossSec() << endl;
@@ -251,9 +251,9 @@ namespace Materials
       TUCNElement* elemAluminium = new TUCNElement("Al", "Aluminium", Aluminium::z, Aluminium::a, Aluminium::scatLength, Aluminium::cohCrossSec, Aluminium::incohCrossSec, Aluminium::absCrossSec);
       TUCNMaterial* matAluminium = new TUCNMaterial("Aluminium", elemAluminium, Densities::aluminium);
       TGeoMedium* aluminium = new TGeoMedium("Aluminium", 1, matAluminium);
+      cout << "Added Material: " << aluminium->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << aluminium->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::aluminium << endl;
       cout << "Coherent Cross Sec: " << elemAluminium->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemAluminium->InCoherentCrossSec() << endl;
@@ -271,9 +271,9 @@ namespace Materials
       TUCNElement* elemHeliumII = new TUCNElement("He", "HeliumII", HeliumII::z, HeliumII::a, HeliumII::scatLength, HeliumII::cohCrossSec, HeliumII::incohCrossSec, HeliumII::absCrossSec);
       TUCNMaterial* matHeliumII = new TUCNMaterial("HeliumII", elemHeliumII, Densities::heliumII);
       TGeoMedium* heliumII = new TGeoMedium("HeliumII", 1, matHeliumII);
+      cout << "Added Material: " << heliumII->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << heliumII->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::heliumII << endl;
       cout << "Coherent Cross Sec: " << elemHeliumII->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemHeliumII->InCoherentCrossSec() << endl;
@@ -291,9 +291,9 @@ namespace Materials
       TUCNElement* elemVacuum = new TUCNElement("H", "Vacuum", Vacuum::z, Vacuum::a, Vacuum::scatLength, Vacuum::cohCrossSec, Vacuum::incohCrossSec, Vacuum::absCrossSec);
       TUCNMaterial* matVacuum = new TUCNMaterial("Vacuum", elemVacuum, Densities::vacuum);
       TGeoMedium* vacuum = new TGeoMedium("Vacuum", 1, matVacuum);
+      cout << "Added Material: " << vacuum->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << vacuum->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::vacuum << endl;
       cout << "Coherent Cross Sec: " << elemVacuum->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemVacuum->InCoherentCrossSec() << endl;
@@ -311,9 +311,9 @@ namespace Materials
       TUCNElement* elemLithium6 = new TUCNElement("H", "Lithium6", Lithium6::z, Lithium6::a, Lithium6::scatLength, Lithium6::cohCrossSec, Lithium6::incohCrossSec, Lithium6::absCrossSec);
       TUCNMaterial* matLithium6 = new TUCNMaterial("Lithium6", elemLithium6, Densities::lithium6);
       TGeoMedium* lithium6 = new TGeoMedium("Lithium6", 1, matLithium6);
+      cout << "Added Material: " << lithium6->GetName() << endl;
       /*
       cout << "---------------------------------" << endl;
-      cout << "Added Material: " << lithium6->GetName() << endl;
       cout << "Density (kg/m^3): " << Densities::lithium6 << endl;
       cout << "Coherent Cross Sec: " << elemLithium6->CoherentCrossSec() << endl;
       cout << "InCoh Cross Sec: " << elemLithium6->InCoherentCrossSec() << endl;
