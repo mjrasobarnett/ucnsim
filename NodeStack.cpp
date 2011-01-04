dae95abcf8b4839ee01af75eb84248e6d8e2a938
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-01-04 12:07:49 +0000

    NodeStack::AddNode now checks if Node is unique
    -- We do not want the same nodes taking multiple places in the node stack

--- NodeStack.cpp
+++ NodeStack.cpp
@@ -42,7 +42,7 @@ bool NodeStack::AddNode(const KDTreeNode& node, const double distance)
    // -- Take pointer to node and its distance from the search-point, and insert
    // -- into node stack if its distance is less than any other node in the stack.
    // -- Maintain ordering of the stack from closest to furthest away. Return whether
-   // -- node was added or not.
+   // -- node was added or not. All nodes in the stack must be unique - no duplicates
    
    // If stack is empty, just add the node
    if (this->empty() == true) {
@@ -56,6 +56,8 @@ bool NodeStack::AddNode(const KDTreeNode& node, const double distance)
    // Otherwise, search node-stack for whether to add node and at what position
    list<StackElement>::iterator listIter;
    for (listIter=this->begin(); listIter!=this->end(); listIter++) {
+      // If node already exists in Stack, do nothing
+      if (node.GetPoint() == listIter->first->GetPoint()) {return false;}
       // Determine whether node's distance is smaller than others in the stack 
       if (distance < listIter->second) {
          // If node is closer, then insert into stack in front of current position
