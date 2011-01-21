590c729ab9159c340de92b8c7fece554268caa51
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-01-21 10:24:55 +0000

    Convert UniformField to global coordinates
    -- Need to convert the supplied vector to global coordinates on definition

--- src/UniformMagField.cxx
+++ src/UniformMagField.cxx
@@ -1,6 +1,9 @@
 // UniformMagField
 // Author: Matthew Raso-Barnett  22/05/2009
+#include <iostream>
+
 #include "TMath.h"
+#include "TGeoMatrix.h"
 #include "UniformMagField.h"
 
 using namespace std;
@@ -9,7 +12,8 @@ ClassImp(UniformMagField)
 
 //_____________________________________________________________________________
 UniformMagField::UniformMagField()
-                :MagField()
+                :MagField(),
+                 fField()
 {
 // Default constructor.
    Info("UniformMagField", "Default Constructor");
@@ -17,11 +21,20 @@ UniformMagField::UniformMagField()
 
 //_____________________________________________________________________________
 UniformMagField::UniformMagField(const string& name, const TVector3& field, const TGeoShape* shape, const TGeoMatrix* matrix)
-                :MagField(name, shape, matrix), 
-                 fField(field)
+                :MagField(name, shape, matrix),
+                 fField()
 {
 // Default constructor.
    Info("UniformMagField", "Constructor");
+   // Convert field vector supplied to global coordinates
+   Double_t global[3], local[3] = {field[0],field[1],field[2]};
+   matrix->LocalToMaster(local, global);
+   // Zero out any rounding errors
+   for (int i = 0; i < 3; i++) {global[i] = (TMath::Abs(global[i]) < 1.E-12) ? 0.0 : global[i];}
+   fField.SetXYZ(global[0],global[1],global[2]);
+   cout << "Built Uniform Magnetic Field - ";
+   cout << "Bx: " << fField[0] << "\t By: " << fField[1];
+   cout << "\t Bz: " << fField[2] << endl;
 }
 
 //_____________________________________________________________________________
