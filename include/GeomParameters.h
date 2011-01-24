bb7f1b88eccaf31cf5f02bfcb63cf7accda7dcc6
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-01-24 14:01:08 +0000

    Setup example run that displays counting problem
    -- We see strange regions where either over-counting or under-counting has occurred.

--- include/GeomParameters.h
+++ include/GeomParameters.h
@@ -308,9 +308,9 @@ namespace GeomParameters {
 namespace FieldParameters {
    // -- Uniform Solenoid
    // X, Y, Z are in the local coord system of the volume
-   const Double_t solenoidBx = 0.;
-   const Double_t solenoidBy = 0.;
-   const Double_t solenoidBz = 5.0*Units::uT;
+   const Double_t solenoidBx = 0.0*Units::uT;
+   const Double_t solenoidBy = 0.01*Units::uT;
+   const Double_t solenoidBz = 0.0*Units::uT;
    
 }
 
