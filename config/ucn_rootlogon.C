230ff378e1f8ef4a18e304215563ca117e50866c
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-02-23 14:27:01 +0000

    Cleaned up some garbage in rootlogon

--- config/ucn_rootlogon.C
+++ config/ucn_rootlogon.C
@@ -1,54 +1,40 @@
 {
-	cout << "rootlogon.C for UCNSIM" << endl;
-	cout << "-------------------------------------" << endl;
-  	if ( gSystem->Load("libPhysics") == 0 ) {
-    cout << "Successfully loaded libPhysics.so" << endl;
-  	} 
-	if ( gSystem->Load("libGeom.so") == 0 ) {
-    cout << "Successfully loaded libGeom.so" << endl;
-  	} 
-  	if ( gSystem->Load("libGeomPainter.so") == 0 ) {
-    cout << "Successfully loaded libGeomPainter.so" << endl;
-  	} 
- 	if ( gSystem->Load("libTree.so") == 0 ) {
-	    cout << "Successfully loaded libTree.so" << endl;
-	}
- 	if ( gSystem->Load("libEG.so") == 0 ) {
-	    cout << "Successfully loaded libEG.so" << endl;
-	}
-	if ( gSystem->Load("libMathCore.so") == 0 ) {
-	    cout << "Successfully loaded libMathCore.so" << endl;
-	}
-	if ( gSystem->Load("libMathMore.so") == 0 ) {
-	    cout << "Successfully loaded libMathCore.so" << endl;
-	}
-	TString ucnsim = gSystem->Getenv("UCN_DIR");
-	if ( ucnsim.Length() == 0 ) {
-		cerr << "-------------------------------------" << endl;
-		cerr << "Warning: Failed to find env. variable UCNSIM" << endl;
-		cerr << "-------------------------------------" << endl;
-	}
-	else {
-		TString ucnlib = ucnsim + "/lib/libUCN.so";
-		cerr << ucnlib.Data() << endl;
-		if ( gSystem->Load(ucnlib.Data()) == 0 ) {
-		   cout << "Successfully loaded libUCN.so" << endl;
-  	   }
-	}
-   
-   TString magdir = gSystem->Getenv("MAGDIR");
-	if ( magdir.Length() == 0 ) {
-		cerr << "-------------------------------------" << endl;
-		cerr << "Warning: Failed to find env. variable MAGDIR" << endl;
-		cerr << "-------------------------------------" << endl;
-	}
-	else {
-		TString maglib = magdir + "/lib/libEDM.so";
-		cerr << maglib.Data() << endl;
-		if ( gSystem->Load(maglib.Data()) == 0 ) {
-		   cout << "Successfully loaded libEDM.so" << endl;
-  	   }
+   cout << "rootlogon.C for UCNSIM" << endl;
+   cout << "-------------------------------------" << endl;
+   if ( gSystem->Load("libPhysics") == 0 ) {
+      cout << "Successfully loaded libPhysics.so" << endl;
+   } 
+   if ( gSystem->Load("libGeom.so") == 0 ) {
+      cout << "Successfully loaded libGeom.so" << endl;
+   } 
+   if ( gSystem->Load("libGeomPainter.so") == 0 ) {
+      cout << "Successfully loaded libGeomPainter.so" << endl;
+   } 
+   if ( gSystem->Load("libTree.so") == 0 ) {
+      cout << "Successfully loaded libTree.so" << endl;
    }
-	cout << "-------------------------------------" << endl;
-	set_my_style();
+   if ( gSystem->Load("libEG.so") == 0 ) {
+      cout << "Successfully loaded libEG.so" << endl;
+   }
+   if ( gSystem->Load("libMathCore.so") == 0 ) {
+       cout << "Successfully loaded libMathCore.so" << endl;
+   }
+   if ( gSystem->Load("libMathMore.so") == 0 ) {
+       cout << "Successfully loaded libMathCore.so" << endl;
+   }
+   TString ucnsim = gSystem->Getenv("UCN_DIR");
+   if ( ucnsim.Length() == 0 ) {
+      cerr << "-------------------------------------" << endl;
+      cerr << "Warning: Failed to find env. variable UCNSIM" << endl;
+      cerr << "-------------------------------------" << endl;
+   }
+   else {
+      TString ucnlib = ucnsim + "/lib/libUCN.so";
+      cerr << ucnlib.Data() << endl;
+      if ( gSystem->Load(ucnlib.Data()) == 0 ) {
+         cout << "Successfully loaded libUCN.so" << endl;
+      }
+   }
+   cout << "-------------------------------------" << endl;
+   set_my_style();
 } 
