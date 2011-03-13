38d8dd194a37cbab37cfdb35dbf1161411265fad
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-13 22:33:15 +0000

    Change simulate_ucn to take a run number argument
    -- By default it will still just take the first run in the master config file, but now you can specify on the command line which run to simulate. 
    -- Also put the lines involved in running the simulation into a separate function

--- src/programs/simulate_ucn.cxx
+++ src/programs/simulate_ucn.cxx
@@ -6,13 +6,17 @@
 #include "RunConfig.h"
 #include "InitialConfig.h"
 #include "Run.h"
+#include "Algorithms.h"
 
 using std::cout;
 using std::endl;
 using std::cerr;
 using std::string;
 
-Int_t main(Int_t argc,Char_t **argv)
+bool PerformSimulation(const ConfigFile& configFile, const int runNumber);
+
+//__________________________________________________________________________
+Int_t main(Int_t argc, Char_t **argv)
 {
    ///////////////////////////////////////////////////////////////////////////////////////
    // -- Set up benchmark
@@ -23,59 +27,41 @@ Int_t main(Int_t argc,Char_t **argv)
    // Build the ConfigFile
    ///////////////////////////////////////////////////////////////////////////////////////
    string configFileName;
+   string suppliedRun = "";
    if (argc == 2) {
-      configFileName= argv[1];
+      configFileName = argv[1];
+      suppliedRun = "1";
+   } else if (argc == 3) {
+      configFileName = argv[1];
+      suppliedRun = argv[2];
    } else {
       cerr << "Error: No configuration file has been specified." << endl;
-      cerr << "Usage, ucnsim <configFile.cfg>" << endl;
+      cerr << "Usage, ucnsim <configFile.cfg> <run no.>" << endl;
       return EXIT_FAILURE;
    }
    ConfigFile configFile(configFileName);
    ///////////////////////////////////////////////////////////////////////////////////////
    // -- Fetch the Number of Runs
    ///////////////////////////////////////////////////////////////////////////////////////
+   int specifiedRunNumber = 0;
+   Algorithms::String::ConvertToInt(suppliedRun, specifiedRunNumber);
+   if (specifiedRunNumber == 0) {
+      cerr << "Invalid run number supplied." << endl;
+      return EXIT_FAILURE;
+   }
    cout << "-------------------------------------------" << endl;
    cout << "Initialising the Runs" << endl;
    cout << "-------------------------------------------" << endl;
-   const Int_t numberOfRuns = configFile.GetInt("NumberOfRuns","Runs");
-   if (numberOfRuns < 1) {
-      cerr << "Cannot read valid number of runs from ConfigFile" << endl;
+   const int numberOfRuns = configFile.GetInt("NumberOfRuns","Runs");
+   if (numberOfRuns < 1 || numberOfRuns < specifiedRunNumber) {
+      cerr << "Cannot read valid number of runs from ConfigFile: " << numberOfRuns << endl;
       return EXIT_FAILURE;
    }
-   cout << "Number of Runs: " << numberOfRuns << endl; 
+   cout << "Number of Runs: " << numberOfRuns << endl;
    ///////////////////////////////////////////////////////////////////////////////////////
    // -- Create the Runs
-   ///////////////////////////////////////////////////////////////////////////////////////
-   for (Int_t runNumber = 1; runNumber <= numberOfRuns; runNumber++) {
-      // Read in the Run Configuration
-      RunConfig runConfig(configFile, runNumber);
-      // Create the Run
-      cout << "-------------------------------------------" << endl;
-      cout << "Creating Run" << runNumber << endl;
-      Run run(runConfig);
-      ///////////////////////////////////////////////////////////////////////////////////////
-      // -- Initialise Run
-      ///////////////////////////////////////////////////////////////////////////////////////
-      if (!(run.Initialise())) {
-         cerr << "Run" << runNumber << " failed to initialise successfully." << endl;
-         return EXIT_FAILURE;
-      }
-      ///////////////////////////////////////////////////////////////////////////////////////
-      // -- Run Simulation
-      ///////////////////////////////////////////////////////////////////////////////////////
-      if (!(run.Start())) {
-         cerr << "Run" << runNumber << " failed to proceed successfully." << endl;
-         return EXIT_FAILURE;
-      }
-      ///////////////////////////////////////////////////////////////////////////////////////
-      // -- Export to File
-      ///////////////////////////////////////////////////////////////////////////////////////
-      if (!(run.Finish())) {
-         cerr << "Run" << runNumber << " failed to finish successfully." << endl;
-         return EXIT_FAILURE;
-      }
-      cout << "Run" << runNumber << " successfully completed." << endl;
-      cout << "-------------------------------------------" << endl;
+   if (PerformSimulation(configFile, specifiedRunNumber) == false) {
+      return EXIT_FAILURE;
    }
    ///////////////////////////////////////////////////////////////////////////////////////
    // -- Output up benchmark
@@ -84,4 +70,40 @@ Int_t main(Int_t argc,Char_t **argv)
    benchmark.Print("UCNSIM");
    cout << "-------------------------------------------" << endl;
    return EXIT_SUCCESS;
+}
+
+//__________________________________________________________________________
+bool PerformSimulation(const ConfigFile& configFile, const int runNumber)
+{
+   // -- Perform a simulation for the given run number
+   // Read in the Run Configuration
+   RunConfig runConfig(configFile, runNumber);
+   // Create the Run
+   cout << "-------------------------------------------" << endl;
+   cout << "Creating Run" << runNumber << endl;
+   Run run(runConfig);
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // -- Initialise Run
+   ///////////////////////////////////////////////////////////////////////////////////////
+   if (!(run.Initialise())) {
+      cerr << "Run" << runNumber << " failed to initialise successfully." << endl;
+      return false;
+   }
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // -- Run Simulation
+   ///////////////////////////////////////////////////////////////////////////////////////
+   if (!(run.Start())) {
+      cerr << "Run" << runNumber << " failed to proceed successfully." << endl;
+      return false;
+   }
+   ///////////////////////////////////////////////////////////////////////////////////////
+   // -- Export to File
+   ///////////////////////////////////////////////////////////////////////////////////////
+   if (!(run.Finish())) {
+      cerr << "Run" << runNumber << " failed to finish successfully." << endl;
+      return false;
+   }
+   cout << "Run" << runNumber << " successfully completed." << endl;
+   cout << "-------------------------------------------" << endl;
+   return true;
 }
