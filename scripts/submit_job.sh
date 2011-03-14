c9828cd692783ccaa80d39230d621a028c0458f1
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-14 14:05:51 +0000

    Fix submit_jobs.sh qsub commands

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+# -- QSUB options
+# When a single command in the array job is sent to a compute node,
+# its task number is stored in the variable SGE_TASK_ID, so we can
+# use the value of that variable to create subdirs or log names
+# that depend on the jobID number
+#
+#$ -cwd
+#$ -o /epp/scratch/nedm/mb325/logs
+#$ -e /epp/scratch/nedm/mb325/logs
+#$ -S /bin/bash
+#$ -q short.q
+
 # -- Make sure that this script is exited if we try to use any uninitialised variable
-set -u
 #
 # -- Fetch arguments from command line
 # -- $# stores the number of comman line arguments passed to shell script
 # -- Arguments accessed by $1, $2, $3...
-if [ "$#" -ne 2 ] ; then
+if [ "$#" -ne 1 ] ; then
    echo "Incorrect number of command line arguments: $@ passed to submit_job.sh"
    exit 1
 else
-   export JOB_DIR="$1"
-   export RUN_NUM="$2"
+   export RUN_NUM="$1"
 fi
 #
 # -- Setup the required environment variables on machine
@@ -23,7 +33,7 @@ export ROOTSYS=${HOME}/root/root
 export PATH=${UCN_DIR}/bin:${ROOTSYS}/bin:${PATH}
 export GSL=${HOME}/gsl/gsl
 export BOOST=${HOME}/boost/boost
-export LD_LIBRARY_PATH=${UCN_DIR}/lib:${ROOTSYS}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${UCN_DIR}/lib:${ROOTSYS}/lib/root:${LD_LIBRARY_PATH}
 export ROOTMACROS=${UCN_DIR}/macros:${ROOTSYS}/macros
 export ROOTALIAS=${UCN_DIR}/config/ucn_rootalias.C
 export ROOTLOGON=${UCN_DIR}/config/ucn_rootlogon.C
@@ -31,45 +41,17 @@ export ROOTLOGOFF=${UCN_DIR}/config/ucn_rootlogoff.C
 echo "ROOT  : $ROOTSYS"
 echo "GSL   : $GSL"
 echo "BOOST : $BOOST"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 echo "------------------------------------------------------------------------"
 echo "UCN_DIR  : $UCN_DIR"
 echo "UCN_GEOM : $UCN_GEOM"
 echo "UCN_RUNS : $UCN_RUNS"
 echo "------------------------------------------------------------------------"
 #
-# -- cd to workarea
-echo "Work Area: $JOB_DIR"
-cd $JOB_DIR
-# Check that move to work area was successful
-if [ "$PWD" -ne "$JOB_DIR" ] ; then
-  echo "Can't change to $JOB_DIR."
-  exit 1
-fi
 #
 # -- Set up Job name and location of master config.cfg
+export CONFIG_FILE="config.cfg"
 export JOB_NAME="run$RUN_NUM"
-export CONFIG_FILE="$JOB_DIR/config.cfg"
-#
-# -- QSUB options
-# When a single command in the array job is sent to a compute node,
-# its task number is stored in the variable SGE_TASK_ID, so we can
-# use the value of that variable to create subdirs or log names
-# that depend on the jobID number
-#
-# set your email address if you want notification
-export JOB_EMAIL="rasobarnettmatthew@gmail.com"
-#$ -M $JOB_EMAIL
-# set the next line to the job title (optional)
-#$ -N $JOB_NAME
-# -- Redirect standard output to scratch
-#$ -o $JOB_DIR
-#$ -e $JOB_DIR
-# -- Redirect standard err to scratch
-# -- specify queue (all.q is the default and needn't be specified)
-export JOB_QUEUE="short.q"
-#$ -q $JOB_QUEUE
-#
-# -- Execute Job
 echo "Simulating Run No: $RUN_NUM"
 simulate_ucn $CONFIG_FILE $RUN_NUM
 #
