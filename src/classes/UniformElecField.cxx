3f5cba226c4928ced0ba0b1e05d8edd02026e4ef
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-20 22:10:59 +0000

    Implement ElecField and UniformElecField classes

---
+++ src/classes/UniformElecField.cxx
@@ -0,0 +1,62 @@
+// UniformElecField
+// Author: Matthew Raso-Barnett  22/05/2009
+#include <iostream>
+
+#include "TMath.h"
+#include "TGeoMatrix.h"
+#include "UniformElecField.h"
+
+#include "Algorithms.h"
+
+using namespace std;
+
+ClassImp(UniformElecField)
+
+//_____________________________________________________________________________
+UniformElecField::UniformElecField()
+                 :ElecField(),
+                  fField()
+{
+// Default constructor.
+   Info("UniformElecField", "Default Constructor");
+} 
+
+//_____________________________________________________________________________
+UniformElecField::UniformElecField(const string& name, const TVector3& field, const TGeoShape* shape, const TGeoMatrix* matrix)
+                 :ElecField(name, shape, matrix),
+                  fField()
+{
+// Default constructor.
+   Info("UniformElecField", "Constructor");
+   // Convert field vector supplied to global coordinates
+   Double_t global[3], local[3] = {field[0],field[1],field[2]};
+   matrix->LocalToMaster(local, global);
+   // Zero out any rounding errors
+   for (int i=0; i<3; i++) {global[i] = Algorithms::IsEqual(global[i], 0.0) ? 0.0 : global[i];}
+   fField.SetXYZ(global[0],global[1],global[2]);
+   cout << "Built Uniform Electric Field - ";
+   cout << "Ex: " << fField[0] << "\t Ey: " << fField[1];
+   cout << "\t Ez: " << fField[2] << endl;
+}
+
+//_____________________________________________________________________________
+UniformElecField::UniformElecField(const UniformElecField& other)
+                 :ElecField(other),
+                  fField(other.fField)
+{
+   // Copy constructor.
+} 
+
+//_____________________________________________________________________________
+UniformElecField::~UniformElecField()
+{
+   // Destructor.
+   Info("UniformElecField", "Destructor");
+}
+
+//______________________________________________________________________________
+const TVector3 UniformElecField::GetField(const Point& /*point*/) const
+{
+   // No position dependence for a Uniform field so return field vector
+   return fField;
+}
