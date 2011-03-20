4707ff5d9376478653f6429bd5b4e0bf65dd27c2
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-20 22:03:50 +0000

    Create FieldArray class
    -- Takes away common FieldArray functionality for Mag and Elec FieldArray classes

--- include/MagFieldArray.h
+++ include/MagFieldArray.h
@@ -1,55 +1,36 @@
 // MagFieldArray
 // Author: Matthew Raso-Barnett  29/09/2010
 
-#ifndef ROOT_MagFieldArray
-#define ROOT_MagFieldArray
+#ifndef MAGFIELDARRAY_H
+#define MAGFIELDARRAY_H
 
-#include "TNamed.h"
 #include <string>
-#include <map>
-#include <iostream>
-#include "TVector3.h"
-
-#include "MagField.h"
-#include "Observable.h"
+#include "FieldArray.h"
 
 ////////////////////////////////////////////////////////////////////////////
 //                                                                        //
 //    MagFieldArray                                                 //
 //                                                                        //
 ////////////////////////////////////////////////////////////////////////////
-using std::string;
-using std::map;
-using std::cout;
-using std::endl;
-
 class Point;
 
-class MagFieldArray : public TNamed, public Observable
+class MagFieldArray : public FieldArray
 {
 private:
-   typedef map<string, MagField*> FieldContainer;
-   FieldContainer fFieldList;
-   
-   void PurgeFields();
-   
-   //** Don't allow copy construction - otherwise we run into problems with ownership of pointers
+   // Don't allow copy construction - otherwise we run into problems with ownership of pointers
    MagFieldArray(const MagFieldArray&);
-   
+
 public:
    // -- constructors
    MagFieldArray();
-   
-   void AddField(MagField* field);
    
    // -- destructor
    virtual ~MagFieldArray();
 
    // -- methods
-   const TVector3 GetMagField(const Point& point, const string = "") const;
-   virtual void   NotifyObservers(const Point& point, const std::string& context);
+   const TVector3 GetMagField(const Point& point, const std::string = "") const;
    
    ClassDef(MagFieldArray, 1)
 };
 
-#endif
+#endif /* MAGFIELDARRAY_H */
---
+++ src/classes/FieldArray.cxx
@@ -0,0 +1,85 @@
+// FieldArray.cxx
+
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+
+#include "Point.h"
+#include "FieldArray.h"
+
+//#define VERBOSE_MODE
+
+using namespace std;
+
+ClassImp(FieldArray)
+
+//_____________________________________________________________________________
+FieldArray::FieldArray()
+           :TNamed("FieldArray","Field Array"),
+            Observable()
+{
+   // Default constructor
+   Info("FieldArray", "Default Constructor");
+}
+
+//_____________________________________________________________________________
+FieldArray::FieldArray(const FieldArray& other)
+           :TNamed(other),
+            Observable(other),
+            fFieldList(other.fFieldList)
+{
+   //copy constructor
+   Info("FieldArray", "Copy Constructor");
+}
+
+//_____________________________________________________________________________
+FieldArray::~FieldArray()
+{
+   // Destructor
+   Info("FieldArray", "Destructor");
+   this->PurgeFields();
+}
+
+//_____________________________________________________________________________
+void FieldArray::PurgeFields()
+{
+   // -- Cleanup container of field pointers
+   FieldContainer::iterator fieldIter;
+   for(fieldIter = fFieldList.begin(); fieldIter != fFieldList.end(); ++fieldIter) {
+       if(fieldIter->second) delete fieldIter->second;
+       fieldIter->second = 0;
+   }
+}
+
+//_____________________________________________________________________________
+void FieldArray::AddField(Field* field)
+{
+   // -- Add field to container
+   fFieldList.insert(pair<string, Field*>(field->GetName(), field));
+}
+
+//_____________________________________________________________________________
+const TVector3 FieldArray::GetField(const Point& point, const string /*volume*/) const
+{
+   // -- Determine which field contains the current particle
+   FieldContainer::const_iterator fieldIter;
+   for(fieldIter = fFieldList.begin(); fieldIter != fFieldList.end(); ++fieldIter) {
+      if (fieldIter->second->Contains(point.GetPosition()) == kTRUE) {
+         return fieldIter->second->GetField(point);
+      }
+   }
+   cout << "NO FIELD FOUND!" << endl;
+   point.GetPosition().Print();
+   return TVector3(0.,0.,0.);
+}
+
+//_____________________________________________________________________________
+void FieldArray::NotifyObservers(const Point& point, const std::string& context)
+{
+   // -- Notify Observers of change
+   for(int i = 0; i < this->CountObservers(); i++) {
+      Observer* observer = this->GetObserver(i);
+      observer->RecordEvent(point, context);
+   }
+}
+
