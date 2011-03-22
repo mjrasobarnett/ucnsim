df61685cc4d2f8dcc1d5ad254471939d5f683de2
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-22 11:23:16 +0000

    Move static Box functions to be protected
    -- Simplifies the interface to only what is necessary

--- include/Box.h
+++ include/Box.h
@@ -14,7 +14,17 @@
 
 class Box : public TGeoBBox 
 {
-
+protected:
+   static Double_t TimeFromInsideS(const Double_t* point, const Double_t* velocity, const Double_t* field,
+    const Double_t dx, const Double_t dy, const Double_t dz, const Double_t *origin, const Bool_t onBoundary);
+   static Double_t TimeFromOutsideS(const Double_t* point, const Double_t* velocity, const Double_t* field,
+    const Double_t dx, const Double_t dy, const Double_t dz, const Double_t *origin, const Bool_t onBoundary);
+   
+   
+   static Bool_t IsNextPointOnBox(const Double_t* point, const Double_t* velocity, const Double_t* field, const Double_t* boundary, const Double_t t);
+   static Double_t SmallestInsideTime(const Int_t solutions, Double_t* roots, const Bool_t onBoundary);
+   static Double_t SmallestOutsideTime(const Int_t solutions, Double_t* roots, const Bool_t onBoundary, const Double_t* point, const Double_t* velocity, const Double_t* field, const Double_t* boundary);
+   
 public:
    // constructors
    Box();
@@ -26,17 +36,9 @@ public:
    
    // methods
    virtual Double_t TimeFromInside(const Double_t* point, const Double_t* velocity, const Double_t* field,
-                               const Double_t stepTime, const Bool_t onBoundary) const;
-   static  Double_t TimeFromInsideS(const Double_t* point, const Double_t* velocity, const Double_t* field,
-   const Double_t dx, const Double_t dy, const Double_t dz, const Double_t *origin, const Bool_t onBoundary);
+    const Double_t stepTime, const Bool_t onBoundary) const;
    virtual Double_t TimeFromOutside(const Double_t* point, const Double_t* velocity, const Double_t* field,
-                               const Double_t stepTime, const Bool_t onBoundary) const;
-   static  Double_t TimeFromOutsideS(const Double_t* point, const Double_t* velocity, const Double_t* field,
-   const Double_t dx, const Double_t dy, const Double_t dz, const Double_t *origin, const Bool_t onBoundary);
-   static  Bool_t IsNextPointOnBox(const Double_t* point, const Double_t* velocity, const Double_t* field, const Double_t* boundary, const Double_t t);
-   static Double_t SmallestInsideTime(const Int_t solutions, Double_t* roots, const Bool_t onBoundary);
-   static Double_t SmallestOutsideTime(const Int_t solutions, Double_t* roots, const Bool_t onBoundary, const Double_t* point, const Double_t* velocity, const Double_t* field, const Double_t* boundary);
-
+    const Double_t stepTime, const Bool_t onBoundary) const;
 
    ClassDef(Box, 1) // Box
 };
