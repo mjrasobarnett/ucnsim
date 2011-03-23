befec9402ba3a6082ef77a22d9e587868646f1f5
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-03-23 16:23:21 +0000

    Update make_T2plot to use DataAnalysis functions

--- src/programs/make_T2plot.cxx
+++ src/programs/make_T2plot.cxx
@@ -64,49 +64,18 @@ Int_t main(int argc, char **argv) {
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Open Data File
    //////////////////////////////////////////////////////////////////////////////////////
-   TFile *file = 0;
-   file = TFile::Open(filename.c_str(), "UPDATE");
-   if (!file || file->IsZombie()) {
-      cerr << "Cannot open file: " << filename << endl;
-      return -1;
-   }
-   file->cd();
-   file->ls();
+   TFile* file = Analysis::DataFile::OpenRootFile(filename,"UPDATE");
    TDirectory * const topDir = gDirectory;
    if (topDir->cd(Folders::particles.c_str()) == false) {
       cerr << "No Folder named: " << Folders::particles << " in data file" << endl;
       return EXIT_FAILURE;
    }
-   cout << "-------------------------------------------" << endl;
-   cout << "Successfully Loaded Data File: " << filename << endl;
-   cout << "-------------------------------------------" << endl;
    TDirectory * const particleDir = gDirectory;
    Analysis::DataFile::CountParticles(particleDir);
    ///////////////////////////////////////////////////////////////////////////////////////
    // Build the ConfigFile
    ///////////////////////////////////////////////////////////////////////////////////////
-   // Navigate to Config Folder   
-   if (topDir->cd(Folders::config.c_str()) == false) {
-      cerr << "No Folder named: " << Folders::config << " in data file" << endl;
-      return EXIT_FAILURE;
-   }
-   // -- Loop over all objects in folder and extract latest RunConfig
-   RunConfig* ptr_config = NULL;
-   TKey *configKey;
-   TIter configIter(gDirectory->GetListOfKeys());
-   while ((configKey = dynamic_cast<TKey*>(configIter.Next()))) {
-      const char *classname = configKey->GetClassName();
-      TClass *cl = gROOT->GetClass(classname);
-      if (!cl) continue;
-      if (cl->InheritsFrom("RunConfig")) {
-         ptr_config = dynamic_cast<RunConfig*>(configKey->ReadObj());
-         break;
-      }
-   }
-   const RunConfig& runConfig = *ptr_config;
-   cout << "-------------------------------------------" << endl;
-   cout << "Successfully Loaded RunConfig" << endl;
-   cout << "-------------------------------------------" << endl;
+   const RunConfig& runConfig = Analysis::DataFile::LoadRunConfig(*file);
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Create a Histogram Dir inside File
    TDirectory* histDir = topDir->mkdir(Folders::histograms.c_str());
