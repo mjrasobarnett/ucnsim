0e9d85a385100802fab326a08ec15d0fc2f42e9d
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-05-26 22:30:01 +0100

    Update part of make_plots with recent changes
    -- Will use make_plots as initial test bed, for updating other analysis functions

--- src/programs/make_plots.cxx
+++ src/programs/make_plots.cxx
@@ -11,6 +11,7 @@
 #include "TGeoManager.h"
 
 #include "RunConfig.h"
+#include "ParticleManifest.h"
 
 #include "Algorithms.h"
 #include "DataAnalysis.h"
@@ -21,13 +22,15 @@ using namespace std;
 //_____________________________________________________________________________
 Int_t main(int argc, char **argv)
 {
-/*
+
+   // Start an interactive root session so we can view the plots as they are made
+   TRint *theApp = new TRint("FittingApp", NULL, NULL);
    ///////////////////////////////////////////////////////////////////////////////////////
    // Plot data needs a minimum of 2 arguments - a data file, and the name of the particle
    // 'state' you wish to include in your histograms.
    if (argc < 3) {
       cerr << "Usage:" << endl;
-      cerr << "plot_data <data.root> <statename>" << endl;
+      cerr << "make_plots <data.root> <statename1> <statename2>..." << endl;
       return EXIT_FAILURE;
    }
    // Read in Filename and check that it is a .root file
@@ -37,40 +40,40 @@ Int_t main(int argc, char **argv)
    // valid state names
    vector<string> statenames;
    for (int i = 2; i < argc; i++) {statenames.push_back(argv[i]);}
-   if (Analysis::DataFile::IsValidStateName(statenames) == false) {
-      cerr << "Error: statenames supplied are not valid" << endl;
-      return EXIT_FAILURE;
-   }
-   // Start an interactive root session so we can view the plots as they are made
-   TRint *theApp = new TRint("FittingApp", NULL, NULL);
+   if (Analysis::DataFile::IsValidStateName(statenames) == false) {return EXIT_FAILURE;}
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Open Data File
-   //////////////////////////////////////////////////////////////////////////////////////
    TFile* file = Analysis::DataFile::OpenRootFile(filename,"UPDATE");
    ///////////////////////////////////////////////////////////////////////////////////////
    // Build the ConfigFile
-   ///////////////////////////////////////////////////////////////////////////////////////
    const RunConfig& runConfig = Analysis::DataFile::LoadRunConfig(*file);
    ///////////////////////////////////////////////////////////////////////////////////////
+   // Load the Geometry
    TGeoManager& geoManager = Analysis::DataFile::LoadGeometry(*file);
    //////////////////////////////////////////////////////////////////////////////////////
-   // -- Navigate to and store folder for each selected state
+   // Load the Particle Manifest
+   const ParticleManifest& manifest = Analysis::DataFile::LoadParticleManifest(*file);
+   manifest.Print();
    //////////////////////////////////////////////////////////////////////////////////////
-   vector<TDirectory*> stateDirs;
-   if (Analysis::DataFile::FetchStateDirectories(*file, statenames, stateDirs) == false) {
-      return EXIT_FAILURE;
-   }
+   // Load the Data Tree
+   TTree* dataTree = Analysis::DataFile::LoadParticleDataTree(*file);
+   //////////////////////////////////////////////////////////////////////////////////////
+   // Get a list of all particle tree indexes for the chosen states
+   vector<int> particleIndexes = manifest.GetListing(statenames).GetTreeIndexes();
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Create a Histogram Director if one doesn't already exist in File
-   TDirectory* histDir = file->mkdir("");
+   TDirectory* histDir = file->mkdir("histograms");
    if (histDir == NULL) {
       // histogram folder already exists
-      file->cd("");
+      file->cd("histograms");
       histDir = gDirectory;
    }
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Particle final state
-   Analysis::FinalStates::PlotFinalStates(histDir, stateDirs, runConfig, geoManager);
+/*   Analysis::FinalStates::PlotFinalState(particleIndexes, dataTree, runConfig);
+   //////////////////////////////////////////////////////////////////////////////////////
+   // -- Particle final state
+   Analysis::FinalStates::DrawFinalPositions(particleIndexes, dataTree, geoManager);
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Polarisation
    if (runConfig.ObserveSpin() == kTRUE) {
@@ -91,10 +94,10 @@ Int_t main(int argc, char **argv)
    if (runConfig.ObserveField() == kTRUE) {
       Analysis::Polarisation::PlotField(histDir, stateDirs, runConfig);
    }
-   //////////////////////////////////////////////////////////////////////////////////////
+*/   //////////////////////////////////////////////////////////////////////////////////////
    // -- Clean up and Finish
    cout << "Finished" << endl;
    theApp->Run();
-*/   return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
 
