73dc9cfea405ec52965aec0f99126888929274c8
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2011-05-31 11:53:28 +0100

    Use boost::program_options in make_plots
    -- This takes over the cmd line options parsing, giving a lot more flexibility
    
    -- We will eventually use this for all executables in this project

--- src/programs/make_plots.cxx
+++ src/programs/make_plots.cxx
@@ -17,30 +17,20 @@
 #include "DataAnalysis.h"
 #include "ValidStates.h"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 using namespace std;
 
 //_____________________________________________________________________________
-Int_t main(int argc, char **argv)
-{
-
+bool make_plots(string filename, vector<string> statenames) {
    // Start an interactive root session so we can view the plots as they are made
    TRint *theApp = new TRint("FittingApp", NULL, NULL);
-   ///////////////////////////////////////////////////////////////////////////////////////
-   // Plot data needs a minimum of 2 arguments - a data file, and the name of the particle
-   // 'state' you wish to include in your histograms.
-   if (argc < 3) {
-      cerr << "Usage:" << endl;
-      cerr << "make_plots <data.root> <statename1> <statename2>..." << endl;
-      return EXIT_FAILURE;
-   }
    // Read in Filename and check that it is a .root file
-   string filename = argv[1];
-   if (Analysis::DataFile::IsRootFile(filename) == false) {return EXIT_FAILURE;}
+   if (Analysis::DataFile::IsRootFile(filename) == false) {return false;}
    // Read in list of states to be included in histogram and check that they are
    // valid state names
-   vector<string> statenames;
-   for (int i = 2; i < argc; i++) {statenames.push_back(argv[i]);}
-   if (Analysis::DataFile::IsValidStateName(statenames) == false) {return EXIT_FAILURE;}
+   if (Analysis::DataFile::IsValidStateName(statenames) == false) {return false;}
    //////////////////////////////////////////////////////////////////////////////////////
    // -- Open Data File
    TFile* file = Analysis::DataFile::OpenRootFile(filename,"UPDATE");
@@ -92,6 +82,70 @@ Int_t main(int argc, char **argv)
    // -- Clean up and Finish
    cout << "Finished" << endl;
    theApp->Run();
+   return true;
+}
+
+//_____________________________________________________________________________
+// A helper function to simplify the printing of command line options.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<T>(cout, " ")); 
+    return os;
+}
+
+//_____________________________________________________________________________
+int main(int argc, char **argv)
+{
+   try {
+      // -- Create a description for all command-line options
+      po::options_description description("Allowed options");
+      description.add_options()
+        ("help", "produce help message")
+        ("datafile", po::value<string>(), "filename of the datafile to be plotted")
+        ("states", po::value< vector<string> >()->multitoken(), "list of states to be included in the plots");
+      ;
+      
+      // -- Create a description for all command-line options
+      po::variables_map variables;
+      po::store(po::parse_command_line(argc, argv, description), variables);
+      po::notify(variables);
+      
+      // -- If user requests help, print the options description
+      if (variables.count("help")) {
+        cout << description << "\n";
+        return 1;
+      }
+      
+      // -- Check whether a datafile was given. If not, exit with a warning
+      if (variables.count("datafile")) {
+        cout << "DataFile name was set to: "
+             << variables["datafile"].as<string>() << "\n";
+      } else {
+        cout << "DataFile name was not set.\n";
+        return EXIT_FAILURE;
+      }
+      
+      // -- Check whether a list of states was given. If not, exit with a warning
+      if (variables.count("states")) {
+        cout << "There are " << variables["states"].as< vector<string> >().size()
+             << " included States which are: " 
+             << variables["states"].as< vector<string> >() << "\n";
+      } else {
+       cout << "No states have been selected.\n";
+       return EXIT_FAILURE;
+      }
+      
+      // -- Call make_plots with the datafile and list of states as arguments
+      make_plots(variables["datafile"].as<string>(), variables["states"].as< vector<string> >());
+   }
+   catch(exception& e) {
+       cerr << "error: " << e.what() << "\n";
+       return 1;
+   }
+   catch(...) {
+       cerr << "Exception of unknown type!\n";
+   }
    return EXIT_SUCCESS;
 }
 
