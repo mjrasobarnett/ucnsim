8a29eda5b6c0188a6acd4c0a881f4ba77607c34f
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2012-02-20 12:26:04 +0000

    Remove setting of LD_LIBRARY_PATH in config script
    -- This isn't necessary since the link path for these libraries is found and defined by CMake when building the project

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -8,21 +8,6 @@ echo "------------------------------------------------------------------------"
 # ROOT & UCN_DIR PATHS
 #------------------------------------------------------------------------------
 export PATH=${UCN_DIR}/build:${UCN_DIR}/scripts:${PATH}
-
-if [[ -n "${LD_LIBRARY_PATH}" ]] ; then
-	export LD_LIBRARY_PATH=${BOOST_ROOT}/stage/lib:${UCN_DIR}/lib:${LD_LIBRARY_PATH}
-else
-	export LD_LIBRARY_PATH=${BOOST_ROOT}/stage/lib:${UCN_DIR}/lib:${ROOTSYS}/lib
-fi
-
-if [[ `uname` == Darwin ]] ; then
-  if [[ -n "${DYLD_LIBRARY_PATH}" ]] ; then
-   export DYLD_LIBRARY_PATH=${BOOST_ROOT}/stage/lib:${UCN_DIR}/lib:${DYLD_LIBRARY_PATH}
-  else
-   export DYLD_LIBRARY_PATH=${BOOST_ROOT}/stage/lib:${UCN_DIR}/lib:${ROOTSYS}/lib
-  fi
-fi
-
 export ROOTMACROS=${UCN_DIR}/macros:${ROOTSYS}/macros
 export ROOTLOGON=${UCN_DIR}/config/ucn_rootlogon.C
 #export ROOTLOGOFF=${UCN_DIR}/config/ucn_rootlogoff.C
