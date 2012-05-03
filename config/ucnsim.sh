9738a2c313434fcb04386b57d866a4b441c6d860
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2012-05-03 14:17:20 +0100

    Fix ucnsim.sh to only print msgs to interactive terminals
    -- 'echo' statements now only after checking for existing $PS1 so that messages aren't printed out when this script is called by another script

--- config/ucnsim.sh
+++ config/ucnsim.sh
@@ -1,11 +1,5 @@
 #------------------------------------------------------------------------------
-# GENERAL
-#------------------------------------------------------------------------------
-echo "------------------------------------------------------------------------"
-echo "Setting up environment for UCNSIM"
-echo "------------------------------------------------------------------------"
-#------------------------------------------------------------------------------
-# ROOT & UCN_DIR PATHS
+# Add build and scripts dir to $PATH and set custom ROOTLOGON macros 
 #------------------------------------------------------------------------------
 export PATH=${UCN_DIR}/build:${UCN_DIR}/scripts:${PATH}
 export ROOTMACROS=${UCN_DIR}/macros:${ROOTSYS}/macros
@@ -13,12 +7,19 @@ export ROOTLOGON=${UCN_DIR}/config/ucn_rootlogon.C
 export ROOTLOGOFF=${UCN_DIR}/config/ucn_rootlogoff.C
 export ROOTALIAS=${UCN_DIR}/config/ucn_rootalias.C
 #------------------------------------------------------------------------------
-echo "ROOTSYS    : " $ROOTSYS
-echo "GSL_DIR    : " $GSL_DIR
-echo "BOOST_ROOT : " $BOOST_ROOT
-echo "------------------------------------------------------------------------"
-echo "UCN_DIR  : " $UCN_DIR
-echo "UCN_GEOM : " $UCN_GEOM
-echo "UCN_RUNS : " $UCN_RUNS
-echo "------------------------------------------------------------------------"
+if [[ -n $PS1 ]]
+then
+    echo "------------------------------------------------------------------------"
+    echo "Setting up environment for UCNSIM"
+    echo "------------------------------------------------------------------------"
+    echo "ROOTSYS    : " $ROOTSYS
+    echo "GSL_DIR    : " $GSL_DIR
+    echo "BOOST_ROOT : " $BOOST_ROOT
+    echo "------------------------------------------------------------------------"
+    echo "UCN_DIR  : " $UCN_DIR
+    echo "UCN_GEOM : " $UCN_GEOM
+    echo "UCN_RUNS : " $UCN_RUNS
+    echo "------------------------------------------------------------------------"
+fi
 #------------------------------------------------------------------------------
+
