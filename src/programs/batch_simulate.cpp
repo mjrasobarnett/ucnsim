eb3d98bc16d1d244c09a50130c64912b41fa7ae2
Matt Raso-Barnett <rasobarnettmatthew@gmail.com>
2012-05-04 15:50:19 +0100

    Fix job name in batch_simulate
    -- job name cannot start with a digit for some reason -_-

--- src/programs/batch_simulate.cpp
+++ src/programs/batch_simulate.cpp
@@ -51,7 +51,7 @@ Int_t main(Int_t argc, Char_t **argv)
       ostringstream runName, jobName;
       runName << "Run" << runNum;
       boost::posix_time::ptime today(boost::posix_time::second_clock::local_time());
-      jobName << today.date().year() << "_" << today.date().month() << "_" << today.date().day() << "_";
+      jobName << "job_" << today.date().year() << "_" << today.date().month() << "_" << today.date().day() << "_";
       jobName << today.time_of_day().hours() << today.time_of_day().minutes() << "_run" << runNum;
       map<string,string> section = configFile.GetSection(runName.str());
       if (section.empty()) {
